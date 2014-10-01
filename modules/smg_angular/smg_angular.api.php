948c64c3ab5f3304b075c6dc3bcf4ff9aed89752
Yevgeny Ananin <euug0701@gmail.com>
2014-10-01 17:45:04 -0500

    #107 Added a hook so that modules that include AngularJS code, can 'register' any custom Angular DOM elements.  This is necessary for an IE8 fix.

--- modules/smg_angular/smg_angular.api.php
+++ modules/smg_angular/smg_angular.api.php
@@ -21,3 +21,21 @@ function hook_smg_angular_add_template() {
 
   return $templates;
 }
+
+/**
+ * Register custom elements.  So if any AngularJS code, anywhere on the site uses
+ * any custom elements, they need to register.  This will be used to fix IE8.
+ * @see https://docs.angularjs.org/guide/ie
+ *
+ * @param array $elements
+ *  An array where the values are custom elements
+ */
+function hook_smg_angular_custom_elements() {
+  // Example
+  $custom_elements = array(
+    'custom1',
+    'custom2',
+    'custom3',
+  );
+  return $custom_elements;
+}
